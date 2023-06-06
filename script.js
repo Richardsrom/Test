1f4ef69995743913c2814fc50d4b64a60319a35e
Ricardo <ricch96@hotmail.com>
2023-06-05 21:56:13 -0600

    Cambios en la barra de nav

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let bars = document.querySelector(".bars");
+let text = document.querySelector(".textoP");
+
+function hide() {
+    text.classList.toggle("hide");
+}
+
 
 bars.onclick = function() {
     let navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
+    hide();
 }
